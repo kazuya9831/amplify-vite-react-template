f082f087bb9d2eac389f61b298ac4456919fc450
Kazuya Fukushima <fukushima@serverworks.co.jp>
2025-03-15 10:57:36 +0900

    auth2

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import { Amplify } from "aws-amplify";
+import { Authenticator } from '@aws-amplify/ui-react';
 import outputs from "../amplify_outputs.json";
 import '@aws-amplify/ui-react/styles.css';
 import "@aws-amplify/ui-react-storage/styles.css";
@@ -9,12 +10,17 @@ import {
   createAmplifyAuthAdapter,
   createStorageBrowser,
 } from '@aws-amplify/ui-react-storage/browser';
+import { signInWithRedirect } from "aws-amplify/auth"
 
 Amplify.configure(outputs);
 
 const { StorageBrowser } = createStorageBrowser({
   config: createAmplifyAuthAdapter(),
 });
+
+signInWithRedirect({
+  provider: { custom: "MicrosoftEntraIDSAML" }
+})
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
